1b689e2b438d06529490d56afaef63b98aff673b
xuanhongfei <xuanhongfei@qq.com>
2023-03-21 22:26:46 +0800

    第一次提交

---
+++ src/main/java/Git_test.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: Git_test
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author: 宣洪飞
+ * @Create: 2023/3/21 - 22:24
+ * @Version: v1.0
+ */
+public class Git_test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
