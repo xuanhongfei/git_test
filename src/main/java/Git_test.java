dcb0cae21ba8ca45d6b69798122aa23c150bae7f
xuanhongfei <xuanhongfei@qq.com>
2023-03-22 09:11:15 +0800

    master commit5

--- src/main/java/Git_test.java
+++ src/main/java/Git_test.java
@@ -13,5 +13,6 @@ public class Git_test {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("master commit test1");
     }
 }
